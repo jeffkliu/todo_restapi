94720b44abd886c58a45a84869d5383bb3625b78
Jeff L <jeffreyliu0@gmail.com>
2018-07-16 21:47:33 -0700

    added delete rest api tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -2,8 +2,8 @@ const expect = require('expect');
 const request = require('supertest');
 const {ObjectID} = require('mongodb');
 
-const {app} = require('./../server');
-const {Todo} = require('./../models/todo');
+const {app} = require('../server');
+const {Todo} = require('../models/todo');
 
 const todos = [{
   _id: new ObjectID(),
@@ -82,6 +82,43 @@ describe('GET /todos/:id', () => {
         expect(res.body.todo.text).toBe(todos[0].text);
       })
       .end(done);
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    var hexId = new ObjectID().toHexString();
+
+    request(app)
+      .get(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 for non-object ids', (done) => {
+    request(app)
+      .get('/todos/123abc')
+      .expect(404)
+      .end(done);
+  });
+});
+
+describe('DELETE /todos/:id', ()=>{
+  it('should remove a todo', (done)=> {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body.todo.text).toBe(todo[0].text);
+      })
+      .end((err,res)=>{
+        if(err) {
+          return done(err);
+        }
+      })
+
+      Todo.findById(todos[0]._id.toHexString()).then((todo)=>{
+        expect(todo).toNotExist();
+        done();
+      }).catch((e)=>done(e));
   });
 
   it('should return 404 if todo not found', (done) => {
