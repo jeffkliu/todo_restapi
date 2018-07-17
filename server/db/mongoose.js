5ffaa3778ffab905c5cd894cfe522eb3fcf1ee1a
Jeff L <jeffreyliu0@gmail.com>
2018-07-16 19:44:36 -0700

    pls work

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGOLAB_NAVY_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGOLAB_BRONZE_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
