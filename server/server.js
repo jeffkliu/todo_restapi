fc57ff11e0d3fcb2fb6e099cb178c46b735d00b8
Jeff L <jeffreyliu0@gmail.com>
2018-07-19 20:09:01 -0700

    added tokens for post method

--- server/server.js
+++ server/server.js
@@ -1,3 +1,14 @@
+var env = process.env.NODE_ENV || 'development';
+console.log('env ********', env);
+
+if(env === 'development') {
+  process.env.PORT = 3000;
+  process.env.MONGOLAB_BRONZE_URI = 'mongodb://localhost:27017/TodoApp';
+} else if (env==='test'){
+  PROCESS.env.PORT = 3000;
+  process.env.MONGOLAB_BRONZE_URI = 'mongodb://localhost:27017/TodoAppTEST';
+}
+
 const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -8,9 +19,11 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
+
+/************** POST /TODO ***********/
 
 app.post('/todos', (req, res) => {
   var todo = new Todo({
@@ -98,8 +111,34 @@ app.patch('/todos/:id', (req,res)=>{
 
 });
 
-app.listen(port, () => {
+/************** POST /users ***********/
+
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token)=> {
+    res.header('x-auth', token).send(user)
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/users', (req, res) => {
+  User.find().then((users) => {
+    res.send({users});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+/************** DO NOT DELETE, LISTENING ON PORT ***********/
+
+app.listen(port, ()=>{
   console.log(`Started up at port ${port}`);
 });
+
 
 module.exports = {app};
